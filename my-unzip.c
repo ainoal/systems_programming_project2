56a89025bfae9601645142678967d17919f4f3d3
ainoal <aino.liukkonen@gmail.com>
2021-04-26 03:13:27 +0300

    my-unzip structure, no real functionality

--- my-unzip.c
+++ my-unzip.c
@@ -2,7 +2,7 @@
 /* CT30A3370 Käyttöjärjestelmät ja systeemiohjelmointi
  * ziplib.c
  * Aino Liukkonen
- * 25.4.2021
+ * 26.4.2021
  */
 /*******************************************************************/
 
@@ -10,9 +10,25 @@
 #include "ziplib.h"
 
 int main (int argc, char **argv) {
+	unsigned long long maxMemory;
+	MAPPED_FILE mappedFile;
 
 	if (argc == 1) {
-		printf("Usage:\n");
+		printf("usage: my-unzip: file1 [file2][...]\n");
+		exit(1);
+	}
+	else {
+		// TODO get "maxMemory" (a function already exists for that)
+
+		// TODO count max buffer size
+
+		// TODO allocate buffer
+
+		// TODO map files (a function already exists for that)
+
+		// TODO unzip
+
+		// TODO remember to free memory
 	}
 
 	return 0;
